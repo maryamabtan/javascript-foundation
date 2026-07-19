518a8f8eda2f12d223e9b5ddc99f76921b19ed55
Maryam Abtan <maryam.abtan315@gmail.com>
2026-07-19 15:52:39 +0330

    Add hiring status to developer card

--- script.js
+++ script.js
@@ -1,5 +1,50 @@
 let nameElement = document.getElementById("name");
 nameElement.innerHTML = "maryam";
 
+let ageElement = document.getElementById("age");
+ageElement.innerHTML = "23";
 
+let jobElement = document.getElementById("job");
+jobElement.innerHTML = "Front-end Developer";
+
+let countryElement = document.getElementById("country");
+countryElement.innerHTML = "Iran";
+
+let userName = "Maryam Abtan";
+
+let age = 23;
+
+let job = "Front-end Developer";
+
+let country = "Iran";
+
+
+
+nameElement.innerHTML = userName;
+
+ageElement.innerHTML = age;
+
+jobElement.innerHTML = job;
+
+countryElement.innerHTML = country;
+
+
+
+if (job == "Front-end Developer") {
+    alert("Front-end + JavaScript Developer");
+} 
+else {
+    alert("Front-end Developer");
+}
+
+let isAvailableForHire = true;
+let statusElement = document.getElementById("status");
+
+if(isAvailableForHire){
+    statusElement.innerHTML = "Available for Hire";
+    statusElement.style.color = "green";
+} else{
+    statusElement.innerHTML = "Not Available";
+    statusElement.style.color = "red"
+}
 
