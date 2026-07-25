a22c812b052636cfec307711705de6f0ea54aec8
Maryam Abtan <maryam.abtan315@gmail.com>
2026-07-26 01:54:52 +0330

    Add game winner logic with if else

--- game.js
+++ game.js
@@ -7,7 +7,23 @@ function play(playerChoice) {
     let computerChoice = choices[randomIndex];
 
     document.getElementById("player").innerHTML = playerChoice;
-
     document.getElementById("computer").innerHTML = computerChoice;
 
+    let result = "";
+
+    if (playerChoice === computerChoice) {
+        result = "Draw";
+    }
+    else if (
+        (playerChoice === "Rock" && computerChoice === "Scissors") ||
+        (playerChoice === "Paper" && computerChoice === "Rock") ||
+        (playerChoice === "Scissors" && computerChoice === "Paper")
+    ) {
+        result = "You Win!";
+    }
+    else {
+        result = "Computer Wins!";
+    }
+
+    document.getElementById("result").innerHTML = result;
 }
