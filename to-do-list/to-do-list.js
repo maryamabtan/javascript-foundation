6fdd653acffead3b7021df0c8a7a66d72527d697
Maryam Abtan <maryam.abtan315@gmail.com>
2026-07-27 21:34:21 +0330

    Prevent adding empty tasks

--- to-do-list/to-do-list.js
+++ to-do-list/to-do-list.js
@@ -3,10 +3,17 @@ function addTask() {
     let taskInput = document.getElementById("taskInput");
     let taskText = taskInput.value;
 
+    if (taskText === "") {
+        alert("Please enter a task.");
+        return;
+    }
+
     let li = document.createElement("li");
 
     li.innerHTML = taskText;
 
     document.getElementById("taskList").appendChild(li);
+
+    taskInput.value = "";
 
 }
