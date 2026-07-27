f4e8181461fe6e62de11ccebc041cf2a2c7be28e
Maryam Abtan <maryam.abtan315@gmail.com>
2026-07-27 21:27:14 +0330

    Add new tasks to todo list

--- to-do-list/to-do-list.js
+++ to-do-list/to-do-list.js
@@ -1,3 +1,12 @@
 function addTask() {
 
+    let taskInput = document.getElementById("taskInput");
+    let taskText = taskInput.value;
+
+    let li = document.createElement("li");
+
+    li.innerHTML = taskText;
+
+    document.getElementById("taskList").appendChild(li);
+
 }
